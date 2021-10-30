c0fe5cdbbd0049eb5ed8f5b35765b231e7e22309
ChakPC <ee190002048@iiti.ac.in>
2021-10-30 23:33:29 +0530

    temp

--- Paterson_Stockmeyer/patersonStockmeyerSerial.cpp
+++ Paterson_Stockmeyer/patersonStockmeyerSerial.cpp
@@ -1,32 +1,10 @@
 // Serial Implementation of Paterson - Stockmeyer algorithm
 #include<bits/stdc++.h>
-#include <complex>
 using namespace std;
 
-struct complexNumber{
-    double Re;
-    double Im;
-    complexNumber(double r = 0.0, double i = 0.0){
-        Re = r;
-        Im = i;
-    }
-    complexNumber operator+(complexNumber operand){
-        complexNumber res;
-        res.Re = Re + operand.Re;
-        res.Im = Im + operand.Im;
-        return res;
-    }
-    complexNumber operator*(complexNumber operand){
-        complexNumber res;
-        res.Re = Re * operand.Re - Im * operand.Im;
-        res.Im = Re * operand.Im + Im * operand.Re;
-        return res;
-    }
-};
-
-vector<vector<complexNumber>> operator+(vector<vector<complexNumber>> &A, vector<vector<complexNumber>> &B){
+vector<vector<complex<double>>> operator+(vector<vector<complex<double>>> &A, vector<vector<complex<double>>> &B){
     int size = A.size();
-    vector<vector<complexNumber>> res(size, vector<complexNumber>(size, {0.0, 0.0}));
+    vector<vector<complex<double>>> res(size, vector<complex<double>>(size, complex<double>(0.0, 0.0)));
     for(int i = 0; i < size; i++){
         for(int j = 0; j < size; j++){
             res[i][j] = A[i][j] + B[i][j];
@@ -35,9 +13,9 @@ vector<vector<complexNumber>> operator+(vector<vector<complexNumber>> &A, vector
     return res;
 }
 
-vector<vector<complexNumber>> operator*(complexNumber coeff, vector<vector<complexNumber>> &matrix){
+vector<vector<complex<double>>> operator*(complex<double> coeff, vector<vector<complex<double>>> &matrix){
     int size = matrix.size();
-    vector<vector<complexNumber>> res(size, vector<complexNumber>(size, {0.0, 0.0}));
+    vector<vector<complex<double>>> res(size, vector<complex<double>>(size, {0.0, 0.0}));
     for(int i = 0; i < size; i++){
         for(int j = 0; j < size; j++){
             res[i][j] = coeff * matrix[i][j];
@@ -46,9 +24,9 @@ vector<vector<complexNumber>> operator*(complexNumber coeff, vector<vector<compl
     return res;
 }
 
-vector<vector<complexNumber>> operator*(vector<vector<complexNumber>> &A, vector<vector<complexNumber>> &B){
+vector<vector<complex<double>>> operator*(vector<vector<complex<double>>> &A, vector<vector<complex<double>>> &B){
     int size = A.size();
-    vector<vector<complexNumber>> res(size, vector<complexNumber>(size, {0.0, 0.0}));
+    vector<vector<complex<double>>> res(size, vector<complex<double>>(size, {0.0, 0.0}));
     for(int i = 0; i < size; i++){
         for(int j = 0; j < size; j++){
             for(int k = 0; k < size; k++){
@@ -59,23 +37,23 @@ vector<vector<complexNumber>> operator*(vector<vector<complexNumber>> &A, vector
     return res;
 }
 
-void printMatrix(vector<vector<complexNumber>> &res){
+void printMatrix(vector<vector<complex<double>>> &res){
     int n = res.size();
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            cout << res[i][j].Re <<" + i"<< res[i][j].Im <<"  ";
+            cout << real(res[i][j]) <<" + i"<< imag(res[i][j]) <<"  ";
         }
         cout << "\n";
     }
 }
 
-vector<vector<vector<complexNumber>>> computePowers(vector<vector<complexNumber>> &inputMatrix, int limit){
+vector<vector<vector<complex<double>>>> computePowers(vector<vector<complex<double>>> &inputMatrix, int limit){
     int sz = inputMatrix.size();
-    vector<vector<vector<complexNumber>>> res(limit+1, vector<vector<complexNumber>>(sz, vector<complexNumber>(sz, {0.0, 0.0})));
-    vector<vector<complexNumber>> temp = inputMatrix;
-    vector<vector<complexNumber>> identity(sz, vector<complexNumber>(sz, {0.0, 0.0}));
+    vector<vector<vector<complex<double>>>> res(limit+1, vector<vector<complex<double>>>(sz, vector<complex<double>>(sz, {0.0, 0.0})));
+    vector<vector<complex<double>>> temp = inputMatrix;
+    vector<vector<complex<double>>> identity(sz, vector<complex<double>>(sz, {0.0, 0.0}));
     for(int i = 0; i < sz; i++){
-        identity[i][i] = {1.0, 0.0};
+        identity[i][i] = complex<double>(1.0, 0.0);
     }
     res[0] = identity;
     res[1] = inputMatrix;
@@ -86,27 +64,27 @@ vector<vector<vector<complexNumber>>> computePowers(vector<vector<complexNumber>
     return res;
 }
 
-vector<vector<complexNumber>> patersonStockmeyerSerial(vector<vector<complexNumber>> &inputMatrix, vector<complexNumber> &coefficients, int polynomialVariable, int polynomialDegree){
+vector<vector<complex<double>>> patersonStockmeyerSerial(vector<vector<complex<double>>> &inputMatrix, vector<complex<double>> &coefficients, int polynomialVariable, int polynomialDegree){
     int inputMatrixSize = inputMatrix.size();
     int degree = coefficients.size() - 1;
 
     // Compute powers of A till p
-    vector<vector<vector<complexNumber>>> powersOfInputMatrix = computePowers(inputMatrix, polynomialVariable);
+    vector<vector<vector<complex<double>>>> powersOfInputMatrix = computePowers(inputMatrix, polynomialVariable);
 
     // Declare resultant matrix
-    vector<vector<complexNumber>> resultantMatrix(inputMatrixSize, vector<complexNumber>(inputMatrixSize, {0.0, 0.0}));
+    vector<vector<complex<double>>> resultantMatrix(inputMatrixSize, vector<complex<double>>(inputMatrixSize, {0.0, 0.0}));
 
     // Horner's loop
     for(int q = polynomialDegree-1; q >= 0; q--){
-        vector<vector<complexNumber>> temp(inputMatrixSize, vector<complexNumber>(inputMatrixSize, {0.0, 0.0}));
+        vector<vector<complex<double>>> temp(inputMatrixSize, vector<complex<double>>(inputMatrixSize, {0.0, 0.0}));
         for(int j = 0; j < polynomialVariable; j++){
-            complexNumber coeff = {0.0, 0.0};
+            complex<double> coeff = {0.0, 0.0};
             int index = polynomialVariable * q + j;
             if(j <= degree) coeff = coefficients[index];
-            vector<vector<complexNumber>> adder = coeff * powersOfInputMatrix[j];
+            vector<vector<complex<double>>> adder = coeff * powersOfInputMatrix[j];
             temp = temp + adder;
         }
-        vector<vector<complexNumber>> adder = resultantMatrix * powersOfInputMatrix[polynomialVariable];
+        vector<vector<complex<double>>> adder = resultantMatrix * powersOfInputMatrix[polynomialVariable];
         resultantMatrix = adder + temp;
     }
 
@@ -118,7 +96,7 @@ int main(){
     // Take Input
     int n;
     cin >> n;
-    vector<vector<complexNumber>> A(n, vector<complexNumber>(n, {0.0, 0.0}));
+    vector<vector<complex<double>>> A(n, vector<complex<double>>(n, {0.0, 0.0}));
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
             double a, b;
@@ -128,13 +106,13 @@ int main(){
     }
     int d;
     cin >> d;
-    vector<complexNumber> coeff(d+1, {0.0, 0.0});
+    vector<complex<double>> coeff(d+1, {0.0, 0.0});
     for(int i=0; i<=d; i++){
         double a, b;
         cin >> a >> b;
         coeff[i] = {a, b};
     }
-    vector<vector<complexNumber>> res = patersonStockmeyerSerial(A, coeff, sqrt(d)+1, sqrt(d)+1);
+    vector<vector<complex<double>>> res = patersonStockmeyerSerial(A, coeff, sqrt(d)+1, sqrt(d)+1);
     printMatrix(res);
     return 0;
 }
