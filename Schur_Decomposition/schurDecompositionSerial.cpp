1f9edf4df5a0414704bcb948305bd567e4541a16
vaibhav <chandravaibhav65@gmail.com>
2021-11-01 23:07:08 +0530

    schur rectified

--- Schur_Decomposition/schurDecompositionSerial.cpp
+++ Schur_Decomposition/schurDecompositionSerial.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 // Function to compute Givens matrix for given i, j
 // Theta computed as such that after rotation, the value at (i, j) becomes 0
-complexMatrix computeGivens(int i, int j, int sizeOfMatrix, complexNumber Aij, complexNumber Ajj) {
+complexMatrix computeGivens(int i, int j, int sizeOfMatrix, complexNumber Aij, complexNumber Ajj)
+{
     // Compute theta
     complexNumber theta = atan((complexNumber(-1.0, 0.0) * Aij) / Ajj);
     complexNumber cosTheta = cos(theta);
@@ -12,7 +13,8 @@ complexMatrix computeGivens(int i, int j, int sizeOfMatrix, complexNumber Aij, c
 
     // Compute Givens matrix
     complexMatrix Givens(sizeOfMatrix, vector<complexNumber>(sizeOfMatrix, complexNumber(0.0, 0.0)));
-    for (int x = 0; x < sizeOfMatrix; x++) {
+    for (int x = 0; x < sizeOfMatrix; x++)
+    {
         Givens[x][x] = complexNumber(1.0, 0.0);
     }
     Givens[i][i] = cosTheta;
@@ -22,28 +24,32 @@ complexMatrix computeGivens(int i, int j, int sizeOfMatrix, complexNumber Aij, c
     return Givens;
 }
 
-vector<complexMatrix> computeQR(complexMatrix& inputMatrix) {
+vector<complexMatrix> computeQR(complexMatrix &inputMatrix)
+{
     int sizeOfMatrix = inputMatrix.size();
-    complexMatrix R(sizeOfMatrix, vector<complexNumber>(sizeOfMatrix, complexNumber(0.0, 0.0)));
+    complexMatrix R=inputMatrix;
     complexMatrix QTranspose = identityMatrix(sizeOfMatrix);
-
-    // Perform rotations to form upper triangular matrix
-    for (int i = 1; i < sizeOfMatrix; i++) {
-        for (int j = 0; j < i; j++) {
-            auto G = computeGivens(i, j, sizeOfMatrix, inputMatrix[i][j], inputMatrix[j][j]);
-            QTranspose = QTranspose * G;
+    for (int i = 1; i < sizeOfMatrix; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            auto G = computeGivens(i, j, sizeOfMatrix, R[i][j], R[j][j]);
+            QTranspose = G * QTranspose;
+            R = G*R;
         }
     }
 
+
     // Compute Q and R
     complexMatrix Q = transposeMatrix(QTranspose);
-    R = QTranspose * inputMatrix;
     return {Q, R};
 }
 
 // Function to perform iterations of Schur Decomposition
-complexMatrix schurDecompositionSerial(complexMatrix& inputMatrix, int numberOfIterations) {
-    for (int i = 0; i < numberOfIterations; i++) {
+complexMatrix schurDecompositionSerial(complexMatrix &inputMatrix, int numberOfIterations)
+{
+    for (int i = 0; i < numberOfIterations; i++)
+    {
         auto QR_Vector = computeQR(inputMatrix);
         auto Q = QR_Vector[0];
         auto R = QR_Vector[1];
@@ -54,13 +60,16 @@ complexMatrix schurDecompositionSerial(complexMatrix& inputMatrix, int numberOfI
 }
 
 // Driver function for testing
-int main() {
+int main()
+{
     // Take Input
     int n;
     cin >> n;
     complexMatrix A(n, vector<complexNumber>(n, {0.0, 0.0}));
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
             double a, b;
             cin >> a >> b;
             A[i][j] = {a, b};
