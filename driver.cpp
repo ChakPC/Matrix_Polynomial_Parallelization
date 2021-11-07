bc04b7bfd9c441803d90b526077d82db37978442
ChakPC <ee190002048@iiti.ac.in>
2021-11-07 22:25:27 +0530

    driver in progress

--- driver.cpp
+++ driver.cpp
@@ -3,6 +3,22 @@
 #include "Schur_Decomposition/schurDecompositionSerial.h"
 #include "Sylvester_Equation_Solver/sylvesterEquationSolverSerial.h"
 
+// Function to compute matrix polynomial using Parlett recurrence
+complexMatrix parlettRecurrenceSerial(complexMatrix &A, vector<complexNumber> &coeff, int numOfIterations, int PS_p, int PS_s){
+    // Schur Decomposition
+    vector<complexMatrix> schur = schurDecompositionSerial(A, numOfIterations);
+    complexMatrix Q = schur[0];
+    complexMatrix T = schur[1];
+
+    // Compute function for diagonal elements
+    int size = Q.size();
+    for(int i=0; i<size; i++){
+        if(i + 1 < size && abs(real(Q[i+1][i])) > 1e-15 && abs(real(Q[i+1][i])) > 1e-15){
+
+        }
+    }
+}
+
 int main(){
     // Take Input
     int n;
@@ -23,7 +39,14 @@ int main(){
         cin >> a >> b;
         coeff[i] = {a, b};
     }
-    
+
+    // Output from Paterson Stockmeyer
+    complexMatrix patersonStockmeyerResult = patersonStockmeyerSerial(A, coeff, sqrt(d)+1, sqrt(d)+1);
+    printMatrix(patersonStockmeyerResult);
+
+    // Output from Parlett Recurrence
+    complexMatrix parlettRecurrenceResult = parlettRecurrenceSerial(A, coeff, 10, sqrt(d)+1, sqrt(d)+1);
+    printMatrix(parlettRecurrenceResult);
 
     return 0;
 }
